4284d8fa4b2aa1fdfc32815394c7bcb6b4b2d407
iloureiro <ignacio.loureiro@endava.com>
2019-01-17 17:05:54 -0300

    3 y 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod first;
 pub mod second;
 pub mod third;
+pub mod fourth;
 
 #[cfg(test)]
 mod tests {
@@ -86,7 +87,39 @@ mod tests {
 }
 
 #[cfg(test)]
-mod testThird {
+mod test_third {
     use super::third::List;
-    
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.append(1).append(2).append(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // Make sure empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+    }
+
+   #[test]
+    fn iter() {
+        let list = List::new().append(1).append(2).append(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
 }
--- src/third.rs
+++ src/third.rs
@@ -30,4 +30,35 @@ impl<T> List<T> {
             head: self.head.as_ref().and_then(|node| node.next.clone()),
         }
     }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head
+            .as_ref() // as a reference to be consumed by option mapping
+            .map(|option_node| &option_node.elem)
+    }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_ref().map(|node| &**node),
+        }
+    }
+}
+
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            //Set the next elem in the iteration to self.next
+            // need to convert next to an option holding a reference to Rc<Node<T>>
+            // type(next_ref) = &Rc<Node<T>>
+            self.next = node.next.as_ref().map(|next_ref| &**next_ref);
+            &node.elem
+        })
+    }
+   
 }
